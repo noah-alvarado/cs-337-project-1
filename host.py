57f772c44adac5602a0b64854180f2aa4a321933
Geogeo357 <geogeo357@gmail.com>
2020-01-24 00:21:09 -0600

    set up approach for getting host info

--- host.py
+++ host.py
@@ -1,2 +1,8 @@
+import nltk
+
 def get_hosts():
-    return 'no hosts found!'
+    tweet = "welcome host thingy Bob"
+    nltk.download('punkt')
+    words = nltk.word_tokenize(tweet)
+    host_index = words.index("host")
+    return words[host_index+ 1] + " " + words[host_index + 2]
