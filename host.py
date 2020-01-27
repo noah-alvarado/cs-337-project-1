c8b0814fd630a4bf08392503d1d2b1c6a66d11e2
Geogeo357 <geogeo357@gmail.com>
2020-01-27 17:20:51 -0600

    Update host.py
    using regular expressions instead of just splitting up the string over spaces to find hose

--- host.py
+++ host.py
@@ -1,9 +1,13 @@
-import nltk
-
+# import nltk
+import re
 
 def get_hosts(tweets):
-    tweet = "welcome host thingy Bob"
-    nltk.download('punkt')
-    words = nltk.word_tokenize(tweet)
-    host_index = words.index("host")
-    return words[host_index + 1] + " " + words[host_index + 2]
+    host_re = re.compile('host [A-z]* [A-z]*')
+    tweet = "welcome host Thingy Bob"
+    # nltk.download('punkt')
+    # words = nltk.word_tokenize(tweet)
+    # host_index = words.index("host")
+    # return words[host_index + 1] + " " + words[host_index + 2]
+    possible_host_match = host_re.match(tweet)
+    possible_host = tweet[possible_host_match.start() + 5, possible_host_match.end()]
+    return possible_host
