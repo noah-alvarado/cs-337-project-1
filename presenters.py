8c8d8756d36ca90d889cd7b6f09f2f87787b3d7e
Noah Alvarado <np3474@gmail.com>
2020-01-29 16:01:09 -0600

    requirements.txt

--- presenters.py
+++ presenters.py
@@ -1,2 +1,16 @@
-def get_presenters():
-    return 'no presenters found!'
+def get_presenters(tweets):
+    keyphrases = [
+        ['will', 'be', 'presenting'],
+        ['will', 'present'],
+        ['presents'],
+        ['present'],
+        ['to', 'present'],
+        ['presented', 'by'],
+        ['presenting'],
+        ['presenting', 'with'],
+        ['presents', 'with'],
+        ['presenters'],
+        ['golden', 'globes', 'presenter']
+    ]
+
+
