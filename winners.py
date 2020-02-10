08518eebe2f02a0f27579677b0466cc0321a623f
Mia <miawillett24@gmail.com>
2020-02-10 17:56:48 -0600

    winners edits

--- winners.py
+++ winners.py
@@ -3,24 +3,26 @@ from reference import WINNER_NOISE
 
 
 def get_winners(tweets, awards):
-    phrases = [['wins'],
-               ['won'],
-               ['Wins'],
-               ['Won'],
-               ['won by'],
-               ['wins', 'Golden', 'Globe', 'for'],
-               ['won', 'Golden', 'Globe', 'for'],
-               ['wins', 'a', 'Golden', 'Globe', 'for'],
-               ['won', 'a', 'Golden', 'Globe', 'for'],
-               ['won', 'the', 'Golden', 'Globe', 'for']
-               ['wins', 'the', 'Golden', 'Globe', 'for']]
+    phrases = [
+        ['wins'],
+        ['won'],
+        ['Wins'],
+        ['Won'],
+        ['won by'],
+        ['wins', 'Golden', 'Globe', 'for'],
+        ['won', 'Golden', 'Globe', 'for'],
+        ['wins', 'a', 'Golden', 'Globe', 'for'],
+        ['won', 'a', 'Golden', 'Globe', 'for'],
+        ['won', 'the', 'Golden', 'Globe', 'for'],
+        ['wins', 'the', 'Golden', 'Globe', 'for']
+    ]
 
     all_winners = dict()
     all_winners['ignore'] = dict()
     for category in awards.keys():
         all_winners[category] = dict()
 
-    for tweetObj in tweets.__dict__.items():
+    for tweetObj in tweets.__dict__.values():
         lowercase = ' '.join([lw for lw in map(lambda x: x.lower(), tweetObj.words)])
         for phrase in map(lambda x: ' '.join(x), phrases):
             if phrase in lowercase:
@@ -29,31 +31,9 @@ def get_winners(tweets, awards):
                     if winners not in all_winners[award]:
                         all_winners[award][winners] = 0
                     if award != 'ignore':
-                        print(award, winners)
+                        print('award: ', award, 'winner: ', winners)
                     all_winners[award][winners] += 1
                 break
-
-            # found_items = [False] * len(phrase)
-            # for i in range(len(tweetObj.words) - len(phrase)):
-            #     seq = tweetObj.words[i:(i + len(phrase))]
-            #     for p in range(len(phrase)):
-            #         if phrase[p].lower() == seq[p].lower():
-            #             found_items[p] = True
-            #         else:
-            #             found_items = [False] * len(phrase)
-            #             break
-            #     if all(found_items):
-            #         break
-            #
-            # if all(found_items):
-            #     winners, award = extract_winners(tweetObj, phrase, awards)
-            #
-            #     winners = '+'.join(winners)
-            #     if winners not in all_winners[award]:
-            #         all_winners[award][winners] = 0
-            #
-            #     all_winners[award][winners] += 1
-            #     break
 
     return "we're trying here"
 
@@ -108,16 +88,18 @@ def extract_winners(tweet, phrase, awards):
 
         possible_awards.append(award_name)
 
+    title_awards = ['best motion picture - drama', 'best motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best television series - comedy or musical', 'best mini-series or motion picture made for television']
+
     for award in possible_awards:
-        # find presenter
         name_match = re.compile("[A-Z][A-z-]* [A-Z][A-z-]*")
+        title_match = re.compile("[A-Z][A-z-]*")
         all_winner = re.findall(name_match, winner_part)
         all_winner = [p for p in map(lambda x: x.lower(), all_winner)]
         winners = []
-        for p in all_winner:
+        for w in all_winner:
             for nw in WINNER_NOISE:
-                if nw not in p:
-                    winners.append(p)
+                if nw not in w.lower():
+                    winners.append(w)
         if len(winners) > 0:
             yield winners, award
         else:
